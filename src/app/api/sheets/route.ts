11e7b2df04a3fa99dec92958b35b002329e6e5b8
kunbi626 <quangbm.star@gmail.com>
2025-11-28 15:49:01 +0700

    Update route.ts

--- src/app/api/sheets/route.ts
+++ src/app/api/sheets/route.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { NextRequest, NextResponse } from 'next/server';
 
-const SHEET_ID = process.env.SHEET_ID || '1QaYK9w3Mo_wIBgOhVsUDw-tEEFNh9IQ4TfCV1bOPpQI';
-const SHEET_URL = 'https://script.google.com/macros/s/AKfycbwlqlmGnZOicz4fofgDLTaYSr_vWJlXmajhL1VxTpOtp2d8ha9jjsnwChEvP5yKwO3q/exec';
+const SHEET_ID = process.env.SHEET_ID || '1kiMyP6PFjLYVkOU4enGppE2HiaVyUe96oGgkAT3HNR8';
+const SHEET_URL = 'https://script.google.com/macros/s/AKfycbz4JSIArn-W0qqjFHver6N4hn7pjh_-NqxngJhRjjzShOc-3fTW8nZ0-KZQV-OWyEIw/exec';
 
 export async function POST(req: NextRequest) {
     try {
